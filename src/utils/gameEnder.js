19f976d128859f5200c89effc265cab29efdef95
dev_vens <vensalex1991@gmail.com>
2025-08-19 11:08:40 +0200

    Enhance endGame function to handle level up and transition to next level

--- src/utils/gameEnder.js
+++ src/utils/gameEnder.js
@@ -1,5 +1,5 @@
 import { showMessage } from "./chronometer.js";
-
+import { gameNextLevel } from "./gameNextLevel.js";
 /**
  * Ends the game and displays the final message.
  * @param {Object} gameStatus - The status of the game at the end.
@@ -7,9 +7,17 @@ import { showMessage } from "./chronometer.js";
  * @param {string} gameStatus.message - The message to display at the end of the game.
  * @param {number} gameStatus.time - The time taken to complete the game.
  * @param {number} gameStatus.score - The final score of the player.
- *
+ * @param {GameService} gameService - L'instance du service de jeu
  */
-const endGame = (gameStatus) => {
+const endGame = (gameStatus, gameService = null) => {
+  if (gameStatus.levelUp && gameService) {
+    showMessage("Niveau supérieur atteint !", false);
+    // Attendre un peu avant de passer au niveau suivant
+    setTimeout(() => {
+      gameNextLevel(gameService, endGame);
+    }, 2000);
+    return;
+  }
   showMessage(gameStatus.message, !gameStatus.win);
 
   // Désactiver les clics sur les cartes
